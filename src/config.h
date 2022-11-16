384b7cdde7e0d6f298b17c50a8b1cce9f8989009
maxstrid-pop <maxstrid@github.io>
2022-11-15 22:58:56 -0800

    Remove unused function

--- src/config.h
+++ src/config.h
@@ -7,6 +7,4 @@
 // Vector only needed because size can't be known at compile time
 std::vector<std::string> read_config(std::string filename);
 
-void parse_config(std::vector<std::string> &config_vec);
-
 #endif // CONFIG_H_
