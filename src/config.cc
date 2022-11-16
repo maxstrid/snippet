aa896df0670395788dd3967d4ed50afdd4354cbd
maxstrid-pop <maxstrid@github.io>
2022-11-16 13:18:10 -0800

    Format files

--- src/config.cc
+++ src/config.cc
@@ -1,11 +1,12 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 #include "config.h"
 
-std::vector<std::pair<std::string, std::string>> read_config(std::string filename) {
+std::vector<std::pair<std::string, std::string>>
+read_config(std::string filename) {
   std::vector<std::pair<std::string, std::string>> line_vec;
 
   std::ifstream file(filename, std::ios::binary);
@@ -17,13 +18,14 @@ std::vector<std::pair<std::string, std::string>> read_config(std::string filenam
   std::string file_str = "";
 
   while (std::getline(file, file_str)) {
-    if(file_str[0] != '#' && !file_str.empty()) {
+    if (file_str[0] != '#' && !file_str.empty()) {
       std::string first = file_str.substr(0, file_str.find(' '));
       std::string second = file_str.substr(first.size(), file_str.find(' '));
-      
-      second.erase(std::remove_if(second.begin(), second.end(), isspace), second.end());
 
-      std::pair<std::string, std::string> pr (first, second);
+      second.erase(std::remove_if(second.begin(), second.end(), isspace),
+                   second.end());
+
+      std::pair<std::string, std::string> pr(first, second);
       line_vec.push_back(pr);
     }
   }
--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,8 @@
-#include <iostream>
-#include <fstream>
 #include <cstring>
-#include <unistd.h>
+#include <fstream>
+#include <iostream>
 #include <limits.h>
+#include <unistd.h>
 #include <vector>
 
 #include "config.h"
@@ -10,50 +10,49 @@
 #define CONFIG "/.config/snippet"
 
 void copy(std::string file, std::string deststr) {
-    std::ifstream source(file, std::ios::binary);
-    std::ofstream dest(deststr, std::ios::binary);
+  std::ifstream source(file, std::ios::binary);
+  std::ofstream dest(deststr, std::ios::binary);
 
-    dest << source.rdbuf();
+  dest << source.rdbuf();
 
-    source.close();
-    dest.close();
+  source.close();
+  dest.close();
 }
 
 int main(int argc, char *argv[]) {
-    std::string home_var = std::getenv("HOME");
+  std::string home_var = std::getenv("HOME");
 
-    if (home_var.empty()) {
-        std::cerr << "Couldn't find environemnt variable $HOME" << std::endl;
-        return 1;
+  if (home_var.empty()) {
+    std::cerr << "Couldn't find environemnt variable $HOME" << std::endl;
+    return 1;
+  }
+
+  std::string screen = home_var + CONFIG + "/Screen.java";
+  std::string runner = home_var + CONFIG + "/Runner.java";
+
+  char cwd[PATH_MAX];
+
+  if (getcwd(cwd, sizeof(cwd)) == nullptr) {
+    std::cerr << "getcwd() error\n";
+    return 1;
+  }
+
+  for (int i = 0; i < argc; i++) {
+    if (strcmp(argv[i], "screen") == 0) {
+      std::cout << "Moving screen\n";
+
+      copy(screen, cwd + std::string("/Screen.java"));
+    } else if (strcmp(argv[i], "runner") == 0) {
+      std::cout << "Moving runner\n";
+
+      copy(runner, cwd + std::string("/Runner.java"));
     }
+  }
 
-    std::string screen = home_var + CONFIG + "/Screen.java";
-    std::string runner = home_var + CONFIG + "/Runner.java";
+  auto config = read_config("example.conf");
 
-    char cwd[PATH_MAX];
-
-    if (getcwd(cwd, sizeof(cwd)) == nullptr) {
-        std::cerr << "getcwd() error\n";
-        return 1;
-    }
-
-    for (int i = 0; i < argc; i++) {
-        if (strcmp(argv[i], "screen") == 0) {
-            std::cout << "Moving screen\n";
-
-            copy(screen, cwd + std::string("/Screen.java"));
-        } else if (strcmp(argv[i], "runner") == 0) {
-            std::cout << "Moving runner\n";
-
-            copy(runner, cwd + std::string("/Runner.java"));
-        }
-    }
-
-
-    auto config = read_config("example.conf");
-
-    for(auto pair: config) {
-        std::cout << pair.first << " : " << pair.second << '\n';
-    }
-    return 0;
+  for (auto pair : config) {
+    std::cout << pair.first << " : " << pair.second << '\n';
+  }
+  return 0;
 }
